2d77aef569201c52f443449a684d259b750fb23f
JeanEspinosa19 <jeanpool.191021@gmail.com>
2026-04-12 18:29:32 -0500

    Corrección de errores en servidor de aplicación

--- CCL-Server/CCL-Server/Program.cs
+++ CCL-Server/CCL-Server/Program.cs
@@ -1,44 +1,103 @@
-using CCL_Server;
-using Microsoft.EntityFrameworkCore;
+    using CCL_Server;
+    using CCL_Server.Security;
+    using CCL_Server.Services;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Http.HttpResults;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.IdentityModel.Tokens;
+    using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+    var key = builder.Configuration["Jwt:Key"];
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+    // Add services to the container.
 
-var origenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(',');
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
 
-builder.Services.AddCors(opt =>
-{
-    opt.AddDefaultPolicy(politica =>
-    {
-        politica.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+    builder.Services.AddSingleton<usuariosTemporales>();
+
+    builder.Services.AddAuthorization();
+    builder.Services.AddAuthentication("Bearer").AddJwtBearer(opt => {
+    
+        var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        opt.RequireHttpsMetadata = false;
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            RoleClaimType = "Rol",
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = signingKey,
+            ClockSkew = TimeSpan.Zero
+        };
+        opt.Events = new JwtBearerEvents
+        {
+            OnChallenge = context =>
+            {
+                context.HandleResponse(); // evita respuesta por defecto
+                if (context.Response.HasStarted)
+                    return Task.CompletedTask;               
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/json";
+
+                string mensaje = "Debes Iniciar Sesión Para Ver la Información o Realizar Cualquier Acción";
+
+                if (context.AuthenticateFailure is SecurityTokenExpiredException)
+                {
+                    mensaje = "Tu Tiempo de Sesión ha Expirado, Vuelve a Iniciar Sesión";
+                }
+
+                return context.Response.WriteAsync($@"{{""message"": ""{mensaje}""}}");
+
+            },
+            OnForbidden = context =>
+            {
+                context.Response.StatusCode = 403;
+                context.Response.ContentType = "application/json";
+
+                return context.Response.WriteAsync(
+                    "{\"message\": \"No Tienes Permisos Para Realizar Esta Acción\"}"
+                );
+            }
+        };
     });
-});
 
-builder.Services.AddDbContext<AplicationDB>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    var origenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(',');
+
+    builder.Services.AddCors(opt =>
+    {
+        opt.AddDefaultPolicy(politica =>
+        {
+            politica.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+        });
+    });
+
+    builder.Services.AddDbContext<AplicationDB>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
-//Middleware CORS
-app.UseCors();
+    //Middleware CORS
+    app.UseCors();
 
-app.UseAuthorization();
 
-app.MapControllers();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-app.Run();
+    app.MapControllers();
+
+    app.Run();
--- CCL-Server/CCL-Server/obj/Debug/net9.0/CCL-Server.AssemblyInfo.cs
+++ CCL-Server/CCL-Server/obj/Debug/net9.0/CCL-Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCL-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ece611e9a179e70677354a8a10f2b8e94c2b9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306e67ec97baae9ff550b4304dc98e1e046ecb05")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCL-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCL-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
